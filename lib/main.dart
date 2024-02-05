066dfbfd93aaa0dd2b2cf87169f96d435b8fcca8
Motoshi Suzuki <101000230+motoshi-suzuki-hp@users.noreply.github.com>
2024-02-05 14:55:43 +0900

    ducks

--- lib/main.dart
+++ lib/main.dart
@@ -24,10 +24,11 @@ class CatListPage extends StatelessWidget {
   const CatListPage({super.key});
   static const List<int> error = [
     100, 101, 102, 103, 
-    200, 201, 202, 203, 204, 205, 206, 207, 208, 226, 
-    300, 301, 302, 303, 304, 305, 307, 308, 
-    400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 420, 421, 422, 423, 424, 425, 426, 428, 429, 431, 444, 450, 451, 497, 498, 499, 
-    500, 501, 502, 503, 504, 506, 507, 508, 509, 510, 511, 521, 522, 523, 525, 530, 599
+    200, 201, 202, 203, 204, 205, 206, 207, 208, 218, 226, 
+    300, 301, 302, 303, 304, 305, 306, 307, 308, 
+    400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 419, 420, 421, 422, 423, 424, 425, 426, 428, 429, 430, 431, 440, 444, 449, 450, 451, 460, 463, 464, 494, 495, 496, 497, 498, 499, 
+    500, 501, 502, 503, 504, 505, 506, 507, 508, 509, 510, 511, 520, 521, 522, 523, 524, 525, 526, 527, 529, 530, 561, 598, 599,
+    999,
     ];
 
   @override
@@ -91,38 +92,56 @@ class CatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('$errorId',
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 30,
+          )
+        ),
+        backgroundColor: Colors.black,
+      ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             SizedBox(
               width: 400,
-              height: 400,
+              height: 700,
               // child: Image.network(
               //     'https://http.dog/$errorId.jpg'
               //     ),
-                child: Column(
+                child: ListView(
                   children: [
                     Image.network(
-                      'https://http.cat/$errorId.jpg',
+                      'https://httpcats.com/$errorId.jpg',
                     ),
                     Image.network(
                       'https://http.dog/$errorId.jpg',
+                    ),
+                    Image.network(
+                      'https://httpducks.com/$errorId.jpg',
                     ),
                   ],
                 ),
             ),
             TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
               child: const Text(
                 'return',
                 style: TextStyle(
-                  fontSize: 24,
+                  fontSize: 30,
                   color: Colors.white,
+                  backgroundColor: Colors.black,
                 ),
               ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+
+              style: TextButton.styleFrom(
+                fixedSize: Size(200, 100),
+                alignment: Alignment.topCenter,
+                )
             ),
           ],
         ),
